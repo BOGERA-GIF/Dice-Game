c555f88a975d87c439da0cf6e3d8dfe24eac3991
BOGERA-GIF <bogerahenry333@gmail.com>
2024-10-12 14:19:19 +0300

    debugged the code

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ if (randomNumber1> randomNumber2) {
     document.querySelector("h1").innerHTML ="Player one wins";
 }
 else if(randomNumber2> randomNumber1){
-    document.querySelector("h1").innnerHTML ="Player two wins";
+    document.querySelector("h1").innerHTML ="Player two wins";
 }
 else {
     document.querySelector("h1").innerHTML ="It's a tie" ;
